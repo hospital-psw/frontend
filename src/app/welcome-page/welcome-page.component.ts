9d342086126b7ab671345564250a35770b395f7a
Nikola Grbovic <101930180+Grbson00@users.noreply.github.com>
2022-10-25 19:13:30 +0200

    feat: add feedback to wellcome page (#2)

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,4 +1,6 @@
+import { getLocaleEraNames } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Feedback } from './feedback';
 
 @Component({
   selector: 'app-welcome-page',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent implements OnInit {
+
+  feedback:Feedback[] = [
+    {user: 'Nikola', text: 'Ovde ide opis'},
+    {user: 'Gravara Grbovic', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis sit suscipit ipsam cupiditate, excepturi quisquam in placeat facere earum enim, odio, nulla rem adipisci? Perspiciatis voluptatum in quibusdam quia qui!'},
+    {user: 'Andrija', text: 'Ja sam mala sisa'},
+  ]
 
   constructor() { }
 
