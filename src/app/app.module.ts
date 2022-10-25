b8f486f875b4e8177bc298e95675081b77713e2f
Nikola Grbovic <101930180+Grbson00@users.noreply.github.com>
2022-10-25 19:52:54 +0200

    Pzp#1.7 feature/feedback on welcome page (#3)
    * feat: add feedback to wellcome page
    
    * feat: add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { PagesModule } from "./modules/pages/pages.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { FeedbackCardComponent } from './welcome-page/feedback-card.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
-    FeedbackCardComponent
+    FeedbackCardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
