c5131d38101036341847e0b8b955cf1ecef40284
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2022-10-26 00:31:21 +0200

    added feedback form component and feedback page

--- src/app/pages/feedback/feedback.module.ts
+++ src/app/pages/feedback/feedback.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeedbackPageComponent } from './components/feedback-page/feedback-page.component';
 import { FeedbackFormComponent } from './components/feedback-form/feedback-form.component';
+import { MaterialModule } from 'src/app/material/material.module';
 
 
 
 @NgModule({
   declarations: [
     FeedbackPageComponent,
-    FeedbackFormComponent
+    FeedbackFormComponent,
   ],
   imports: [
+    MaterialModule,
     CommonModule
   ]
 })
