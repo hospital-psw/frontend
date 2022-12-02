ef08da0d84bccbbaf690aa94042104fd9e5c599e
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2022-12-02 15:18:44 +0100

    added layout for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './modules/pages/home/home.component';
 import { ChangePasswordComponent } from './pages/bloodbank-password/components/change-password/change-password.component';
 import { FeedbackPageComponent } from './pages/feedback/components/feedback-page/feedback-page.component';
 import { RegistrationFormComponent } from './pages/registration/components/registration-form/registration-form/registration-form.component';
@@ -8,10 +7,11 @@ import { LoginPageComponent } from './pages/login/components/login-page/login-pa
 import { WelcomePageComponent } from './pages/welcome/components/welcome-page/welcome-page.component';
 import { AuthGuard } from './shared/Auth/guard/auth.guard';
 import { ProfilePageComponent } from './pages/patient-profile/components/profile-page/profile-page.component';
+import { HomePageComponent } from './pages/home/components/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: WelcomePageComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomePageComponent},
   { path: 'feedback', component: FeedbackPageComponent, canActivate:[AuthGuard]},
   { path: 'changePassword', component: ChangePasswordComponent },
   { path: 'registration', component: RegistrationFormComponent},
