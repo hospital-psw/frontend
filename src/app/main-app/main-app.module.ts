a92f56b308f969c55608e12919592ac52664e796
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2022-12-15 02:24:36 +0100

    changed app logo in navbar

--- src/app/main-app/main-app.module.ts
+++ src/app/main-app/main-app.module.ts
@@ -4,9 +4,6 @@ import { RouterModule } from '@angular/router';
 import { MainAppComponent } from './main-app.component';
 import { MainAppRoutingModule } from './main-app-routing.module'
 import { NavbarComponent } from '../navbar/navbar.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { LoaderModule } from '../shared/modules/loader/loader.module';
-
 
 @NgModule({
   declarations: [MainAppComponent, NavbarComponent],
